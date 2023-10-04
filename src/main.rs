1351578cb65b10331dfcba34cfa4cbf0fbe2c7ed
nyakiomaina <carolinenyakio21@gmail.com>
2023-10-04 12:23:43 +0300

    rough idea?

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use tendermint_abci::ServerBuilder;
 use tendermint_proto::abci::{
     RequestInfo, ResponseInfo, 
     RequestQuery, ResponseQuery, 
     RequestCheckTx, ResponseCheckTx, 
-    ResponseCommit
+    ResponseCommit,
 };
 
 use std::collections::HashMap;
 use tendermint_abci::Application;
-
+// use tendermint_proto::v0_34::proto::v0_37::abci::RequestDeliverTx;
 #[derive(Clone)]
 struct KeyValueStore {
-    _storage: HashMap<String, String>,
+    storage: HashMap<String, String>,
 }
 
 impl Application for KeyValueStore {
@@ -18,13 +19,38 @@ impl Application for KeyValueStore {
         ResponseInfo::default()
     }
     
-    fn query(&self, _req: RequestQuery) -> ResponseQuery {
-        ResponseQuery::default()
+    fn query(&self, req: RequestQuery) -> ResponseQuery {
+        let key = String::from_utf8_lossy(&req.data).to_string();
+        match self.storage.get(&key) {
+            Some(value) => {
+                let mut response = ResponseQuery::default();
+                response.value = value.clone().into_bytes().into();
+                response
+            }
+            None => ResponseQuery::default(),
+        }
     }
 
-    fn check_tx(&self, _req: RequestCheckTx) -> ResponseCheckTx {
-        ResponseCheckTx::default()
+    fn check_tx(&self, req: RequestCheckTx) -> ResponseCheckTx {
+        if req.tx.is_empty() {
+            let mut response = ResponseCheckTx::default();
+            response.code = 1;
+            response.log = "Empty TX".into();
+            response
+        } else {
+            ResponseCheckTx::default()
+        }
     }
+
+    // fn deliver_tx(&self, req: RequestDeliverTx) -> RequestDeliverTx {
+    //     let parts: Vec<&[u8]> = req.tx.split(|b| *b == b'=').collect();
+    //     if parts.len() == 2 {
+    //         let key = String::from_utf8_lossy(parts[0]);
+    //         let value = String::from_utf8_lossy(parts[1]);
+    //         self.storage.insert(key.to_string(), value.to_string());
+    //     }
+    //     ResponseDeliverTx::default()
+    // }
 
     fn commit(&self) -> ResponseCommit {
         ResponseCommit::default()
@@ -33,8 +59,12 @@ impl Application for KeyValueStore {
 
 #[tokio::main]
 async fn main() {
-    let _app = KeyValueStore {
-        _storage: HashMap::new(),
+    let app = KeyValueStore {
+        storage: HashMap::new(),
     };
-   // todo: server implementation
+   // server implementation
+   let server_builder = ServerBuilder::default();
+   let server = server_builder.bind("127.0.0.1:26658", app).expect("Failed to binf server to address");
+
+   server.listen().expect("Failed to listen for connections");
 }
